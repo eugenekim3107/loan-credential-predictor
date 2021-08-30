bdad059bf51c409fb82890036241331521073a22
Eugene Kim <eugenekim3107@gmail.com>
2021-08-29 21:37:12 -0700

    Created all initial folders and files for project

--- notebooks/exploratory.py
+++ notebooks/exploratory.py
@@ -3,14 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-loan_data = pd.read_csv('loan_data.csv', sep = ',')
+loan = pd.read_csv('loan_data.csv', sep = ',')
 
 #Understand the data
-loan_data.head()
-loan_data.info()
+loan.head()
+loan.info()
+loan.describe()
 
 #visualize the data
+loan.hist(bins=50, figsize=(20,15))
+plt.show()
 
+#stratify data using FICO score
+from sklearn.model_selection import StratifiedShuffleSplit
+
+loan['fico_cat'] = pd.cut(loan['fico'],bins=[0,675,700,725,750,850],labels=[1,2,3,4,5])
+loan['fico_cat'].hist()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2,random_state=42)
+
+for train_index, test_index in split.split(loan, loan['fico_cat']):
+    strat_train_set = loan.loc[train_index]
+    strat_test_set = loan.loc[test_index
 
 #split data into training set and testing set
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop('fico_cat', axis=1, inplace=True)
 
+loan_train_set = strat_train_set.copy()
