638186f43b60ebf24e97e9815787a1a5b3082e0e
Eugene Kim <eugenekim3107@gmail.com>
2021-09-10 03:56:07 -0700

    minor fixes

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,3 +3,6 @@ import pandas as pd
 #url to generate data from github
 url = 'https://raw.githubusercontent.com/eugenekim3107/LoanProject/main/data/raw/loan_data.csv'
 loan = pd.read_csv(url, sep=",")
+
+#not necessary for this project
+loan = loan.drop(['not.fully.paid'],axis=1)
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -12,6 +12,9 @@ from sklearn.base import BaseEstimator, TransformerMixin
 url = 'https://raw.githubusercontent.com/eugenekim3107/LoanProject/main/data/raw/loan_data.csv'
 loan = pd.read_csv(url, sep=",")
 
+#not necessary for this project
+loan = loan.drop(['not.fully.paid'], axis=1)
+
 #stratify data using 'fico' feature
 loan['fico_cat'] = pd.cut(loan['fico'], bins = [0,675,700,725,750,850], labels=[1,2,3,4,5])
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
@@ -22,7 +25,7 @@ for set_ in (strat_train_set, strat_test_set):
     set_.drop('fico_cat', axis=1, inplace=True)
 
 #split features and label
-loan = strat_train_set.drop(['credit.policy','not.fully.paid'],axis=1)
+loan = strat_train_set.drop(['credit.policy'],axis=1)
 loan_labels = strat_train_set['credit.policy'].copy()
 
 #use mean to fill in empty cells
